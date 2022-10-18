cb83c7d6ca422d0e36234f64d0805e9df53e05be
Gaute Lorentsen <gudelo97@gmail.com>
2022-10-18 17:49:27 +0200

    Removed gallery from gitignore

---
+++ src/gallery/index/index.js
@@ -0,0 +1,44 @@
+import nidelva from '../nidelva-sunset.jpg';
+import nidelvaTemp from '../nidelva-sunset-temp.jpg';
+import munkholmen from '../m-holmen-summer.jpg';
+import munkholmenTemp from '../m-holmen-summer-temp.jpg';
+import hauklandBeach from '../haukland-beach.jpg';
+import hauklandBeachTemp from '../haukland-beach-temp.jpg';
+import nidarosAurora from '../nidaros-aurora.jpg';
+
+const gallery = [
+  {
+    title: 'Nidelva Sunset',
+    src: nidelva,
+    srcTemp: nidelvaTemp,
+    size: '40x60',
+    price: 699,
+    quantity: 0,
+  },
+  {
+    title: 'Munkholmen Sunset',
+    src: munkholmen,
+    srcTemp: munkholmenTemp,
+    size: '40x60',
+    price: 699,
+    quantity: 0,
+  },
+  {
+    title: 'Haukland Beach',
+    src: hauklandBeach,
+    srcTemp: hauklandBeachTemp,
+    size: '40x60',
+    price: 699,
+    quantity: 0,
+  },
+  {
+    title: 'Aurora over Nidaros',
+    src: nidarosAurora,
+    srcTemp: '',
+    size: '40x60',
+    price: 699,
+    quantity: 0,
+  },
+];
+
+export default gallery;
